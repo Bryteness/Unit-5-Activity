75c5449cce1a15a343782a26ca5f562b8b6b73ac
Bryteness <69054686+Bryteness@users.noreply.github.com>
2023-05-07 14:03:44 -0700

    Add files via upload

---
+++ comment8or/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+
+class Comment8orAdmintSite(admin.AdminSite):
+    index_title = 'c8admin'
+    title_header = 'c8 site admin'
+    site_header = 'c8admin'
+    logout_template = 'comment8or/logged_out/html'
+    
